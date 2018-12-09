ad015722fdc9c718b0c719587fe35741a1438ba6
Szymon Kielch <kielch.szymon@outlook.com>
2018-12-09 13:37:42 +0100

    DbFirstStep

---
+++ DataAccess/Data/FirstFantasyDBContext.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Data
+{
+    public class FirstFantasyDBContext : DbContext
+    {
+        public DbSet<LevelText> LevelsTexts { get; set; }
+
+        public FirstFantasyDBContext(DbContextOptions<FirstFantasyDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LevelText>().ToTable("LevelsTexts");
+        }
+    }
+}
---
+++ DataAccess/Models/LevelText.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataAccess.Models
+{
+    public class LevelText
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string StoryText { get; set; }
+        public int StoryLevel { get; set; }
+
+        [StringLength(10)]
+        public string LineColor { get; set; }
+    }
+}
